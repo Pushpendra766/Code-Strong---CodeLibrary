a77e2a31f388a28ca9ebb9ab375e47daaedf2d10
Pushpendra Chandravanshi <56391001+Pushpendra766@users.noreply.github.com>
2020-10-28 15:55:36 +0530

    Create pascals_triangle.cpp

---
+++ C++/pascals_triangle.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int fact(int n){
+    int fact=1;
+    for(int i=2;i<=n;i++){
+        fact*=i;
+    }
+    return fact;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<fact(i)/(fact(i-j)*fact(j))<<" ";
+            
+        }
+        cout<<endl;
+    }
+    return 0;
+    
+}
