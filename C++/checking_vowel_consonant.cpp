6d11363e2ef53ad0fc00100d38b8e205850dd701
Pushpendra Chandravanshi <56391001+Pushpendra766@users.noreply.github.com>
2020-10-28 15:51:04 +0530

    Create checking_vowel_consonant.cpp

---
+++ C++/checking_vowel_consonant.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char a;
+    cin>>a;
+    if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+    cout<<"Vowel";
+    else
+    cout<<"Consonant";
+    
+    return 0;
+}
