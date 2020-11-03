26dd2a06dadb5f1bfdeae231ae1f7766eb9c2d34
Pushpendra Chandravanshi <56391001+Pushpendra766@users.noreply.github.com>
2020-11-03 19:04:49 +0530

    Create max_sum_of_subarray.cpp

---
+++ C++ questions asked by companies/max_sum_of_subarray.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int currsum[n+1];
+    currsum[0]=0;
+    for(int i=1;i<=n;i++){
+        currsum[i]=currsum[i-1]+a[i-1];
+    }
+    int maxSum=0;
+    for(int i=1;i<=n;i++){
+        int sum=0;
+        for(int j=0;j<i;j++){
+            sum=currsum[i]-currsum[j];
+            maxSum=max(sum,maxSum);
+        }
+    }
+    
+     cout<<maxSum;
+    
+ return 0;   
+}    
