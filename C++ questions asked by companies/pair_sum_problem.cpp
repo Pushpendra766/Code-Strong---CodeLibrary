bcfa1f5f717a27e2aee63f7a844f1c6072703c2b
Pushpendra Chandravanshi <56391001+Pushpendra766@users.noreply.github.com>
2020-11-03 19:07:14 +0530

    Create pair_sum_problem.cpp

---
+++ C++ questions asked by companies/pair_sum_problem.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+bool pairsum(int a[],int n,int k){
+    int low=0,high=n-1;
+    while(low<high)
+    {
+        if(a[low]+a[high]==k){
+            cout<<low<<" "<<high<<endl;
+            return 1;
+        }
+       
+        else if(a[low]+a[high]<k)
+        low++;
+        else{
+            high--;
+        }
+    }
+    return 0;
+}
+int main(){
+    int a[]={2,4,7,11,14,16,20,21};
+    int k=31;
+    cout<<pairsum(a,8,k);
+   
+   
+    
+        
+    return 0;
+}
