d8251dc6881889db4461754928e283c2a5dcb123
Pushpendra Chandravanshi <56391001+Pushpendra766@users.noreply.github.com>
2020-11-30 13:29:03 +0530

    Create recursion factorial.cpp

---
+++ C++/recursion factorial.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int fact(int n){
+    if(n==1)
+    return 1;
+    int prevfact=fact(n-1);
+    return (n*prevfact);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<fact(n);
+    return 0;
+}
