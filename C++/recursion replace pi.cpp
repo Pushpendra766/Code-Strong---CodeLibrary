770a6f1336438ef4dcb18ad3b64e9f45a3735dc8
Pushpendra Chandravanshi <56391001+Pushpendra766@users.noreply.github.com>
2020-12-01 20:24:58 +0530

    Create recursion replace pi.cpp

---
+++ C++/recursion replace pi.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void replacepi(string s){
+    if(s.size()==0)
+    return;
+    
+    if(s[0]=='p'&&s[1]=='i'){
+        cout<<"3.14";
+        replacepi(s.substr(2));
+    }
+    else
+    {
+        cout<<s[0];
+        replacepi(s.substr(1));
+    }
+}
+int main()
+{
+    replacepi("pihjashppiaspijhdpijsjhk");
+    return 0;
+}
