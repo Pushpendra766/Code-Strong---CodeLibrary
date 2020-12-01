a8e49c85e77cf7b4ee0cf478830c7ed70724ef64
Pushpendra Chandravanshi <56391001+Pushpendra766@users.noreply.github.com>
2020-12-01 20:36:06 +0530

    Create recursion removeduplicate.cpp

---
+++ C++/recursion removeduplicate.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+string removedup(string s){
+    if(s.length()==0){
+        return " ";
+    }
+    char ch=s[0];
+    string ans = removedup(s.substr(1));
+    if(ch==ans[0]){
+        return ans;
+    }
+    return (ch+ans);
+}
+int main()
+{
+    cout<<removedup("aaabbbhhhggjkkklal");
+    return 0;
+}
